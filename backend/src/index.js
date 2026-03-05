7454957039fd38846dc9c90eaad4f0829665be5a
Hariom-99 <hariomkhalghat@gmail.com>
2026-03-05 10:02:00 +0530

    created login logout and update profile routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+
 dotenv.config()
 import { connectDB } from "./lib/db.js";
 
 const app=express();
 app.use(express.json());
-
+app.use(cookieParser());
 const PORT=process.env.PORT
 
 
