62efeeacdaceaac0b582b7ceaf315d775ba962d7
Hariom-99 <hariomkhalghat@gmail.com>
2026-03-06 23:26:08 +0530

    setuped the message route : to do implemente the socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,7 @@ const PORT=process.env.PORT
 
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT,()=>{
     console.log("server is running on port ",PORT);
